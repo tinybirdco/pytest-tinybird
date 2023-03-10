e5c4bb527d320d5154c30c3d82ddae71fd42a554
Juan Madurga <jlmadurga@gmail.com>
2023-03-10 10:22:52 +0100

    don't send report from xdist workers

--- tests/test_tinybird.py
+++ tests/test_tinybird.py
@@ -7,16 +7,21 @@ def test_report_to_tinybird(testdir):
     # create a temporary pytest test module
     testdir.makepyfile("""
         import pytest
+        from random import randint
         def test_passed():
             assert True
-    """)
+        def test_flaky():
+            assert randint(1,10) >=5
+        """)
 
     tinybird_url = os.environ["TINYBIRD_URL"] = 'https://fake-api.tinybird.co'
     datasource_name = os.environ["TINYBIRD_DATASOURCE"] = "test_datasource"
     tinybird_token = os.environ["TINYBIRD_TOKEN"] = 'test_token'
 
     with mock.patch('requests.post') as mock_post:
+        mock_post.return_value.status_code = 202
         testdir.runpytest(
+            "-n 2",
             '--report-to-tinybird',
             '-vvv'
         )
