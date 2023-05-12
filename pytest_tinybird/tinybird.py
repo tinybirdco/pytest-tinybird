794304298bccb1c8436ee65992d87a0ca3ff5bab
Jesús Botella <42716-jesusbotella@users.noreply.gitlab.com>
2023-05-12 15:53:48 +0200

    Add branch parameter to report properties

--- pytest_tinybird/tinybird.py
+++ pytest_tinybird/tinybird.py
@@ -22,11 +22,14 @@ class TinybirdReport:
         self.datasource_name = os.environ.get("TINYBIRD_DATASOURCE")
         self.token = os.environ.get("TINYBIRD_TOKEN")
         self.url = f"{self.base_url}/v0/events?name={self.datasource_name}&token={self.token}"
-        self.branch = os.environ.get('CI_MERGE_REQUEST_SOURCE_BRANCH_NAME', os.environ.get('CI_COMMIT_BRANCH', 'ci_commit_branch_unknown'))
         self.commit = os.environ.get('CI_COMMIT_SHA', 'ci_commit_sha_unknown')
         self.job_id = os.environ.get('CI_JOB_ID', 'ci_job_id_unknown')
         self.job_url = os.environ.get('CI_JOB_URL', 'job_url_unknown')
         self.job_name = os.environ.get('CI_JOB_NAME', 'job_name_unknown')
+        self.branch = os.environ.get(
+            'CI_MERGE_REQUEST_SOURCE_BRANCH_NAME',
+            os.environ.get('CI_COMMIT_BRANCH', 'ci_commit_branch_unknown')
+        )
 
     def report(self, session: Session):
         if None in [self.base_url, self.datasource_name, self.token]:
@@ -53,7 +56,8 @@ class TinybirdReport:
                         'test_name': test.head_line,
                         'test_part': test.when,
                         'duration': test.duration,
-                        'outcome': test.outcome
+                        'outcome': test.outcome,
+                        'branch': self.branch
                     })
                 except AttributeError:
                     pass
